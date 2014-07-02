c56728a080433eb452023fe5ea4fb55cda621d8a
Andrew Amundson <andrewamundson0@gmail.com>
2014-07-01 19:39:07 -0700

    styled flash messages

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,44 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+	test "user should enter first name" do
+	  	user = User.new   
+	  	assert !user.save
+	  	assert !user.errors[:first_name].empty?
+  	end
+
+  	test "user should enter last name" do
+	  	user = User.new   
+	  	assert !user.save
+	  	assert !user.errors[:last_name].empty?
+  	end
+
+  	test "user should enter profile name" do
+	  	user = User.new   
+	  	assert !user.save
+	  	assert !user.errors[:profile_name].empty?
+  	end
+
+  	test "user should enter a unique profile name" do
+	  	user = User.new 
+	  	user.profile_name = 'andrew' 
+	  	user.email =  "drew@cif3r.com"
+	  	user.first_name = "andrew"
+	  	user.last_name = "amundson"
+	  	user.password = "password"
+	  	user.password_confirmation = "password"
+	  	
+	  	assert !user.save
+	  	assert !user.errors[:profile_name].unique?
+  	end
+
+  	test "user should enter a profile name without spaces."
+  		user = User.new
+  		user.profile_name = 'Name with spaces.'
+
+  		assert !user.save
+  		assert !user.errors[:profile_name].empty?
+  		assert user.errors[:profile_name].include?("Must be formatted correctly.")
+  	end
 end
