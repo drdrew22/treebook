a8d5dffb319fbdd43a37bae230b4bf8b1c4a5f73
Andrew Amundson <andrewamundson0@gmail.com>
2014-07-01 22:14:38 -0700

    added further testing for users and statuses

--- test/controllers/statuses_controller_test.rb
+++ test/controllers/statuses_controller_test.rb
@@ -23,7 +23,14 @@ class StatusesControllerTest < ActionController::TestCase
     assert_response :success
   end
 
-  test "should create status" do
+  test "should be logged in to post a status" do
+    post :create, status: { content: "hello" }
+    assert_response :redirect
+    assert_redirected_to new_user_session_path
+  end
+
+  test "should create status when logged in" do
+    sign_in users(:andrew)
     assert_difference('Status.count') do
       post :create, status: { content: @status.content }
     end
@@ -37,16 +44,19 @@ class StatusesControllerTest < ActionController::TestCase
   end
 
   test "should get edit" do
+    sign_in users(:andrew)
     get :edit, id: @status
     assert_response :success
   end
 
   test "should update status" do
+    sign_in users(:andrew)
     patch :update, id: @status, status: { content: @status.content }
     assert_redirected_to status_path(assigns(:status))
   end
 
   test "should destroy status" do
+    sign_in users(:andrew)
     assert_difference('Status.count', -1) do
       delete :destroy, id: @status
     end
