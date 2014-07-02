66db46226f18a0aa473dc4b2d220d967dd871100
Andrew Amundson <andrewamundson0@gmail.com>
2014-07-01 21:35:37 -0700

    added new tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,27 +1,28 @@
-class User < ActiveRecord::Base
-  # Include default devise modules. Others available are:
-  # :token_authenticatable, :confirmable,
-  # :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
 
-  # Setup accessible (or protected) attributes for your model
-  attr_accessible :email, :password, :password_confirmation, :remember_me,
-                  :first_name, :last_name, :profile_name
-  # attr_accessible :title, :body
+  class User < ActiveRecord::Base
+    # Include default devise modules. Others available are:
+    # :token_authenticatable, :confirmable,
+    # :lockable, :timeoutable and :omniauthable
+    devise :database_authenticatable, :registerable,
+           :recoverable, :rememberable, :trackable, :validatable
 
-  validates :first_name, presence: true
-  validates :last_name, presence: true
-  validates :profile_name, presence: true, 
-                           uniqueness: true,
-                           format: {
-                            with: /a-zA-Z0-9_-/,
-                            message: 'Must be formatted correctly.'
-                           }
+    # Setup accessible (or protected) attributes for your model
+    attr_accessible :email, :password, :password_confirmation, :remember_me,
+                    :first_name, :last_name, :profile_name, :full_name
+    # attr_accessible :title, :body
 
-  has_many :statuses
+    # validates :first_name, presence: true
+    # validates :last_name, presence: true
+    # validates :profile_name, presence: true, 
+    #                          uniqueness: true,
+    #                          format: {
+    #                           with: /a-zA-Z0-9_-/,
+    #                           message: 'Must be formatted correctly.'
+    #                          }
 
-  def full_name
-  	first_name + " " + last_name
+    has_many :statuses
+
+    def full_name
+    	return "#{first_name} #{last_name}"
+    end
   end
-end
